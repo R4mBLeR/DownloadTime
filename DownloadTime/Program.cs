d083cb0ae4e8a4f8e31ec50ae6be65b3d797369b
Michael <misharad2008@gmail.com>
2023-06-28 17:07:11 +0300

    v1.1

---
+++ DownloadTime/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("" +
+    "*****************************************\n" +
+    "Created by R4mBLe_\n" +
+    "Write file size in GB\n" +
+    "And download speed in Mbps/s\n" +
+    "*****************************************\n");
+double speed;
+double size;
+string size_;
+string speed_;
+Console.WriteLine("Enter file size: ");
+size_ =Console.ReadLine();
+size_ = size_.Replace('.', ',');
+size = Double.Parse(size_);
+Console.WriteLine("Enter download speed: ");
+speed_ = Console.ReadLine();
+speed_ = speed_.Replace('.', ',');
+speed = Double.Parse(speed_);
+speed = speed * 0.1;
+
+int hours = Convert.ToInt32(((size*1000)/speed)/3600);
+int minutes = Convert.ToInt32(((size * 1000) / speed) % 3600 / 60);
+int seconds = Convert.ToInt32(((size * 1000) / speed) % 3600 % 60);
+Console.WriteLine($"The file will download in {hours} hours {minutes} minutes {seconds} seconds");
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,4 @@
+# DownloadTime
+Console app to get file download time!
+
+.exe file in \release
